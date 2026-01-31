a8c1cb960a6c0ff51bf0b2a0d8ebdfd43e86784c
Sergey Mikhno <sergey.mikhno@gmail.com>
2026-01-31 12:34:58 +0100

    export functions

--- src/ocr_reflow/__init__.py
+++ src/ocr_reflow/__init__.py
@@ -7,6 +7,8 @@ and consistent spacing.
 
 from .main import process_document, Letter, find_rects, margins
 from .reflow import create_page_with_word_wrapping
+from .divide_conquer_4d import Point4D, divide_conquer_4d
+from layout import layout
 
 __version__ = "0.1.0"
 __all__ = [
